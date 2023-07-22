7be893b72b17ef4c5bcc9b3160c286013ad3f030
Derek Wheeler <frozenarrow73@gmail.com>
2023-07-21 23:46:03 -0600

    added basic pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import Home from "./screens/Home"
+import Login from "./screens/Login"
+import Register from "./screens/Register"
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
     </div>
   );
 }
