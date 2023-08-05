864949b1a6558aa0cfa11ecde449ae5e7e080740
Derek Wheeler <frozenarrow73@gmail.com>
2023-08-05 10:34:08 -0600

    changed styling for homepage to linear gradient

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,16 +4,17 @@ import AuthContext from "../store/AuthContext"
 function Home() {
   const authContext = useContext(AuthContext)
   return (
-    <div>
+    <div className='homeBackground'>
         <header className='homeHeader'>
             <button className='authButton'>Register</button>
             <h1>My Tarot Journal</h1>
             <button className='authButton'>Login</button>
         </header>
         <body className='homeBody'>
+          <div className='homeContainer'>
             <h2 className='homeH2'>Welcome to My Tarot Journal</h2>
             <p>My Tarot Journal is an all inclusive website for tarot and oracle journaling. Where you can record the symbolic meaning of each card in your tarot decks. You can record your readings and interpretations. Finally you will be able to search through you readings making all that information accessable to you. Empowering you to make new connections, see patterns and learn the tarot or oracle cards on a deeper level then ever before! Making an account is free. Please enjoy.</p>
-            <p>{authContext.test}</p>
+          </div>
         </body>
     </div>
   )
