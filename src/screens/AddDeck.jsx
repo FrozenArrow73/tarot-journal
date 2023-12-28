743bfa4352f5a157365472fe2d1518a8082966e8
Derek Wheeler <frozenarrow73@gmail.com>
2023-12-28 16:58:46 -0700

    added form to add deck page

--- src/screens/AddDeck.jsx
+++ src/screens/AddDeck.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Background from '../components/Background'
 import Header from '../components/Header'
+import BackButton from '../components/BackButton'
 
 function AddDeck() {
+    const backButton = <BackButton destination={"/decks"}/>
   return (
     <Background>
-        <Header/>
+        <Header backButton={backButton}/>
+        <div className='createDeck'>
+            <form className='addDeckForm'>
+                <label htmlFor='deckName'>Name</label>
+                <input type="text" id="deckName"/>
+                <label htmlFor="deckImage">Cover Image</label>
+                <input type="file" id="deckImage"/>
+                <button className='defaultButton'>Submit</button>
+            </form>
+        </div>
     </Background>
   )
 }
